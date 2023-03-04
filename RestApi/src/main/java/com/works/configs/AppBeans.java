dd8cd557f3c140e38316a0233a1d6e16331ee9dd
Hakan ÖZER <hakanozer02@gmail.com>
2023-03-04 11:56:46 +0300

    Cache Manager

--- RestApi/src/main/java/com/works/configs/AppBeans.java
+++ RestApi/src/main/java/com/works/configs/AppBeans.java
@@ -1,10 +1,15 @@
 package com.works.configs;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @Configuration
 public class AppBeans {
@@ -17,6 +22,18 @@ public class AppBeans {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager manager = new SimpleCacheManager();
+        manager.setCaches(
+                Arrays.asList(
+                     new ConcurrentMapCache("product"),
+                     new ConcurrentMapCache("note")
+                )
+        );
+        return manager;
     }
 
 
--- RestApi/src/main/java/com/works/services/ProductService.java
+++ RestApi/src/main/java/com/works/services/ProductService.java
@@ -3,6 +3,8 @@ package com.works.services;
 import com.works.entities.Product;
 import com.works.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,15 +17,18 @@ import java.util.Map;
 public class ProductService {
 
     final ProductRepository repository;
+    final CacheManager cacheManager;
 
     public ResponseEntity save(Product product) {
         Map<String, Object> hm = new LinkedHashMap<>();
         repository.save(product);
         hm.put("status", true);
         hm.put("result", product);
+        cacheManager.getCache("product").clear();
         return new ResponseEntity(hm, HttpStatus.OK);
     }
 
+    @Cacheable("product")
     public ResponseEntity list() {
         Map<String, Object> hm = new LinkedHashMap<>();
         hm.put("status", true);
